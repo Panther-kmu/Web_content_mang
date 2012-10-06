753ce9011ce109033bdbb06892572f9bbc993fbd
Panther-kmu <genuine.kmu@gmail.com>
2012-10-06 08:58:08 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebContent::Application.config.secret_token = 'ab67c8f85f75d59ff791e6e5f38a4cea4f1e4d4f0b4be3c554ab50ca73674d90ea6124ae43f74ea05a9c7ef1c8cb1bad8e7e145df832bb1adee85321c3abbb85'
