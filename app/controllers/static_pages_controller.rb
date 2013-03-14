feeac23aef2a7e68d06e36ce2bbfb4122e612a17
Panther-kmu <genuine.kmu@gmail.com>
2013-03-14 09:44:28 +0530

    removed deleted files

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,12 +2,16 @@ class StaticPagesController < ApplicationController
   #layout false
   def home
     @site_domain = request.env["SERVER_NAME"]
-    @site = Site.find_by_domain(@site_domain)
-    if @site == nil
-      redirect_to :controller=>'home',:action=>'signup'
+    if @site_domain == "localhost"
+      return
     else
-      session[:site] = @site
-      redirect_to :controller=>'site',:action=>'home'
+      @site = Site.find_by_domain(@site_domain)
+      if @site == nil
+        redirect_to :controller=>'home',:action=>'signup'
+      else
+        session[:site] = @site
+        redirect_to :controller=>'site',:action=>'home'
+      end
     end
   end
 
--- app/models/web_user.rb
+++
@@ -1,3 +0,0 @@
-class WebUser < ActiveRecord::Base
-  attr_accessible :email, :name, :password
-end
